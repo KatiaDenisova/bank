637d36b560bde4e6d70492ebd366a2e892fec514
Dzianisava_KV <Dzianisava_KV@st.by>
2019-05-14 18:03:50 +0300

    3 fragments

---
+++ app/src/main/java/com/example/bank/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.bank;
+
+import android.graphics.Paint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        TextView textView;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_pament_services);
+
+//        textView = findViewById(R.id.signIn);
+//        textView.setPaintFlags(textView.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
+
+
+    }
+}
